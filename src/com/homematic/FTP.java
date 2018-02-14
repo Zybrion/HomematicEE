4018ec6f8d2ddcff9d6aa9b7039d55797d442aaf
granaderainer <jonas.koch35@gmail.com>
2018-02-14 11:51:21 +0100

    Hallo

--- src/com/homematic/FTP.java
+++ src/com/homematic/FTP.java
@@ -1,5 +1,13 @@
 package com.homematic;
 
 
+import org.apache.commons.net.ftp.FTPClient;
+
 class FTP{
+
+    public static boolean uploadFile()
+    {
+        FTPClient client = new FTPClient();
+        return false;
+    }
 }
